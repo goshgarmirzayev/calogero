6a4d278cdbe6072fd067b280a765fcaf23b7c3b2
Goshgar <qosqarmirze@gmail.com>
2020-03-18 18:26:15 +0400

    Init

---
+++ src/main/java/com/goshgarmirzayev/calogero/CalogeroApplication.java
@@ -0,0 +1,13 @@
+package com.goshgarmirzayev.calogero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalogeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalogeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goshgarmirzayev/calogero/CalogeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goshgarmirzayev.calogero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalogeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
